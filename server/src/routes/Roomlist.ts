5805266e503937c373999f32952469597c5b879c
ad105geppetto <ad105geppetto@gmail.com>
2022-06-01 09:05:44 +0900

    [init] server init 2nd

--- server/src/routes/Roomlist.ts
+++ server/src/routes/Roomlist.ts
@@ -1,2 +1,9 @@
-const router = 1
-export default router
+import express = require("express");
+import controller from "../controllers/Roomlist"
+const router = express.Router();
+
+router.get("/", controller.get);
+router.post("/", controller.post);
+router.put("/", controller.put);
+router.delete("/", controller.delete);
+export default router;
