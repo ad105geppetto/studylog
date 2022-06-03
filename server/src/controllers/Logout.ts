4f36c50a0477fc0f839950290a38e40004a47a5a
o9oja <chc8909@gmail.com>
2022-06-03 13:20:10 +0900

    [feat] Add logout

---
+++ server/src/controllers/Logout.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+
+export default {
+  get: (req: Request, res: Response, next: NextFunction) => {
+    if (req.headers["authorization"]) {
+      res.send({ accessToken: null, refreshToken: null, message: "로그아웃되었습니다." });
+    }
+  },
+};
