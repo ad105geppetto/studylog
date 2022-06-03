0e82fbe4794c5d9eebc3421ba8ad0fa455bca4b7
o9oja <chc8909@gmail.com>
2022-06-03 13:01:30 +0900

    [feat] Add userinfo

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,17 @@
-import express from "express"
-import loginRouter from "./Login"
-import signupRouter from "./Signup"
-import findPwdMailingRouter from "./FindPwdMailing"
-import checkRouter from "./Check"
-import userIdRouter from "./UserId"
-import userpwdRouter from "./Userpwd"
-import oauthRouter from "./Oauth"
-import roomlistRouter from "./Roomlist"
+import express from "express";
+import loginRouter from "./Login";
+import signupRouter from "./Signup";
+import findPwdMailingRouter from "./FindPwdMailing";
+import checkRouter from "./Check";
+import userIdRouter from "./UserId";
+import userpwdRouter from "./Userpwd";
+import oauthRouter from "./Oauth";
+import roomlistRouter from "./Roomlist";
+////////////////////////////////////////////////////
+
+import userinfoRouter from "./Userinfo";
+
+////////////////////////////////////////////////////
 const router = express.Router();
 
 router.use("/login", loginRouter);
@@ -17,5 +22,10 @@ router.use("/userid", userIdRouter);
 router.use("/userpwd", userpwdRouter);
 router.use("/userpwd/auth", oauthRouter);
 router.use("/roomlist", roomlistRouter);
+///////////////////////////////////////////////////
 
-export default router
+router.use("/userinfo", userinfoRouter);
+
+// router.use("/signup", signupRouter);
+
+export default router;
