355280ffee804099506161bc18d4bf528f39b26e
ad105geppetto <ad105geppetto@gmail.com>
2022-06-03 12:15:22 +0900

    [feat] Update post password auth mailing API
    Send an email so that the authentication number is stored in the database.

--- server/src/controllers/FindPwdMailing.ts
+++ server/src/controllers/FindPwdMailing.ts
@@ -1,4 +1,5 @@
 import nodeMailer from "nodemailer"
+import pwdMailing from "../models/FindPwdMailing"
 import dotenv from "dotenv"
 dotenv.config();
 
@@ -12,12 +13,13 @@ const mailPoster = nodeMailer.createTransport({
   }
 });
 
-const mailOpt = (user_data, title, contents) => {
+const mailOpt = (user_data, title, contents, html) => {
   const mailOptions = {
     from: process.env.MAIL,
     to: user_data,
     subject: title,
-    text: contents
+    text: contents,
+    html: html
   };
 
   return mailOptions;
@@ -38,15 +40,43 @@ const sendMail = (mailOption) => {
 export default {
   post: (req, res) => {
     const email = req.body.email
-    const result = [{ dataValues: email }]
-    let title = "비밀번호 조회에 대한 6자리 숫자 입니다."
-    let random = String(Math.floor(Math.random() * 1000000))
+    const data = [{ dataValues: email }]
+    let title = "[Studylog] 고객님의 인증코드는 다음과 같습니다."
+    let randomAuth = String(Math.floor(Math.random() * 1000000))
     const contents = () => {
-      return random
+      return `${randomAuth}`
     }
-    const mailOption = mailOpt(result[0].dataValues, title, contents());
-    sendMail(mailOption)
+    const html = `
+      <div>
+        <div>
+          <div style="float: left">
+            <img src="https://cdn.pixabay.com/photo/2015/04/15/02/43/waves-723178_960_720.png" width="100" height="100" />
+          </div>
+          <div>
+            <span style="font-size: 1.8rem">함께 공부하고 기록할 땐</span>
+            <br />
+            <span style="font-size: 1.8rem">Studylog</span>
+          </div>
+        </div>
+        <br />
+        <h2>고객님의 인증코드입니다.</h2>
+        <h3>${email}님 안녕하세요,</h3>
+        <h3>고객님의 인증코드는 다음과 같습니다.</h3>
+        <br />
+        <div>
+          <span>인증코드 : </span>
+          <span>${randomAuth}</span>
+        </div>
+      </div>`
 
-    res.json({ message: "ok" })
+    const mailOption = mailOpt(data[0].dataValues, title, contents(), html);
+    sendMail(mailOption)
+    pwdMailing.create(randomAuth, email, 0, (error, result1) => {
+      if (error) {
+        res.status(500).json({ message: "Internal Sever Error" });
+      } else {
+        res.status(200).json({ message: "ok" });
+      }
+    })
   }
 }
