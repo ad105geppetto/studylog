f4454c866c4ab6af8e583785f899435a8f54012f
ad105geppetto <ad105geppetto@gmail.com>
2022-06-05 20:04:22 +0900

    [fix] Update post Oauth API
    I couldn't bring access token so I modified it. The cause was the problem of not using content-type.

--- server/src/controllers/Oauth.ts
+++ server/src/controllers/Oauth.ts
@@ -6,23 +6,16 @@ const clientSecret = process.env.GITHUB_CLIENT_SECRET;
 
 export default {
   post: (req, res) => {
-    axios({
-      method: "post",
-      url: `https://oauth2.googleapis.com/token`,
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      withCredentials: true,
-      data: {
-        code: req.body.authorizationCode,
+    const code = req.body.authorizationCode
+    axios
+      .post(`https://oauth2.googleapis.com/token`, {
+        code: code,
         client_id: clientID,
         client_secret: clientSecret,
-        redirect_uri: "http//localhost:3000",
+        redirect_uri: "http://localhost:3000",
         grant_type: "authorization_code"
-      },
-    })
+      })
       .then((response) => {
-        console.log(response.data);
         const accessToken = response.data.access_token;
         res.status(200).json({ accessToken: accessToken, response: response.data, message: "ok" });
       })
