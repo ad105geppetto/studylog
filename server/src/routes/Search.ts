0ac2cfc905ee657c259e7823b3116a9d1228d2a0
ad105geppetto <ad105geppetto@gmail.com>
2022-06-06 18:25:39 +0900

    [feat] Update get search API

--- server/src/routes/Search.ts
+++ server/src/routes/Search.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import controller from "../controllers/Search.ts"
+import controller from "../controllers/Search"
 const router = express.Router();
 
 router.get("/", controller.get);
