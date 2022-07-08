7f646ac95c6aa1630481405b62f535a5176b763c
o9oja <chc8909@gmail.com>
2022-07-08 15:01:27 +0900

    [fix] Update Todo

--- server/src/models/Todo.ts
+++ server/src/models/Todo.ts
@@ -8,8 +8,8 @@ export default {
     });
   },
 
-  post: (content: string, type: string, tokenData: any, callback: Function) => {
-    const queryString = `INSERT INTO todos (userId, content , type) VALUES (${tokenData.id}, "${content}", "${type}")`;
+  post: (content: string, type: string, index: string, tokenData: any, callback: Function) => {
+    const queryString = `INSERT INTO todos (userId, content , type, index) VALUES (${tokenData.id}, "${content}", "${type}", "${index}")`;
     db.query(queryString, (error, result) => {
       const queryString2 = `SELECT * from todos WHERE userId = ${tokenData.id}`;
       db.query(queryString2, (error, result) => {
@@ -18,8 +18,15 @@ export default {
     });
   },
 
-  patch: (todoId: number, content: string, type: string, tokenData: any, callback: Function) => {
-    const queryString = `UPDATE todos SET content = "${content}", type = "${type}" WHERE id = ${todoId}`;
+  patch: (
+    todoId: number,
+    content: string,
+    type: string,
+    index: string,
+    tokenData: any,
+    callback: Function
+  ) => {
+    const queryString = `UPDATE todos SET content = "${content}", type = "${type}" index = "${index} WHERE id = ${todoId}`;
     db.query(queryString, (error, result) => {
       const queryString2 = `SELECT * from todos WHERE userId = ${tokenData.id}`;
       db.query(queryString2, (error, result) => {
